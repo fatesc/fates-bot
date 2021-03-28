8776a2a02415ad9db30dc8ff767e691c5a2a29f8
fate <66224951+fatesc@users.noreply.github.com>
2021-03-28 11:41:02 +0100

    transfer cmd

--- src/Client.ts
+++ src/Client.ts
@@ -59,20 +59,22 @@ client.on("ready", async () => {
             console.log("config created")
         })
     })
-    const config: guildconfig.RootObject = JSON.parse(await readFile(join(__dirname, "../config.json"), "utf8"))
+    .then(async () => {
+        const config: guildconfig.RootObject = JSON.parse(await readFile(join(__dirname, "../config.json"), "utf8"))
 
-    const ids = client.guilds.cache.map(guild => guild.id).filter(id => !Object.keys(config.guilds).includes(id));
-
-    for (const id of ids) {
-        const guild = await client.guilds.fetch(id, true, true);
-        const newServer = {
-            [id]: ({...(config).guilds["0"]})
+        const ids = client.guilds.cache.map(guild => guild.id).filter(id => !Object.keys(config.guilds).includes(id));
+    
+        for (const id of ids) {
+            const guild = await client.guilds.fetch(id, true, true);
+            const newServer = {
+                [id]: ({...(config).guilds["0"]})
+            }
+            newServer[id].name = guild.name
+            
+            writeFile(join(__dirname, "../config.json"), JSON.stringify({ guilds: {...Object.assign(config.guilds, newServer)} }))
+            .catch(err => console.error(err));
         }
-        newServer[id].name = guild.name
-        
-        writeFile(join(__dirname, "../config.json"), JSON.stringify({ guilds: {...Object.assign(config.guilds, newServer)} }))
-        .catch(err => console.error(err));
-    }
+    })
 });
 
 const db: ConnectionOptions = {
---
+++ src/Commands/Fates Admin/transfer.ts
@@ -0,0 +1,37 @@
+import { Command, user } from "../../types";
+import { Message } from "discord.js";
+import { AsyncQuery } from "../Util/Query";
+import { ResultSetHeader } from "mysql2";
+import { helpCommand } from "../Util/HelpCommand";
+import { MessageEmbed } from "discord.js";
+
+module.exports = {
+    name: "transferwl",
+    description: "transfers another users whitelist to another user",
+    usage: "transferwl [user1] [user2]",
+    run(message: Message, args: string[]) {
+        const user1 = Array.from(message.mentions.users.values())[0]
+        const user2 = Array.from(message.mentions.users.values())[1]
+        if (!user1 || !user2) return helpCommand(message, this.name, `${message.member}, Invalid Command Usage\n`);
+
+        AsyncQuery<Array<user>>("SELECT fingerprint, `key` FROM whitelist.user WHERE discord_id = ?", [user1.id])
+        .then(res => res?.[0])
+        .then(res => {
+            if (res.fingerprint && res.key) {
+                AsyncQuery<ResultSetHeader>("INSERT INTO whitelist.user(`key`,`fingerprint`,`discord_id`,`discord_tag`) VALUES(?,?,?,?)",
+                    [res.key, res.fingerprint, user2.id, user2.tag]
+                )
+                .then(res => {
+                    if (res.affectedRows > 1) {
+                        AsyncQuery<ResultSetHeader>("DELETE FROM whitelist.user WHERE discord_id = ?", [user1.id]);
+                        message.channel.send(new MessageEmbed()
+                            .setTitle("Completed")
+                            .setAuthor(message.author.displayAvatarURL({ dynamic: true }))
+                            .setDescription(`transfered ${user1}'s whitelist to ${user2}`)
+                        )
+                    }
+                })
+            }
+        })
+    }
+} as Command
