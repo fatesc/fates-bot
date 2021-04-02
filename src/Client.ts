13e18bdd8550ba0c089f4c515f7c199a15cca2cc
fate <66224951+fatesc@users.noreply.github.com>
2021-04-02 18:52:27 +0100

    ready event

--- src/Client.ts
+++ src/Client.ts
@@ -1,7 +1,7 @@
 import { Client, Collection, Intents } from "discord.js"
-import { readdir, readFile, stat, writeFile } from "fs/promises"
+import { readdir, stat } from "fs/promises"
 import { join } from "path";
-import { Command, guildconfig } from "./types";
+import { Command } from "./types";
 
 require("dotenv").config({ path: join(__dirname, "../.env") });
 
@@ -37,38 +37,9 @@ deepsearch(join(__dirname + "/Commands/"), (file) => {
     console.log(`${command.name ?? file.split("\\")[file.split("\\").length - 1]} (${type}) is ready!`);
 });
 
-client.on("ready", async () => {
-    console.log(`${client.user.tag} is ready ✔`);
-    client.user.setActivity("fates admin", {type: "PLAYING"});
-    deepsearch(join(__dirname + "/Events/"), (file) => {
-        require(file).default();
-        console.log(`Event ${(file.split("\\")[file.split("\\").length - 1] ?? file.split("/")[file.split("/").length - 1]).replace(/\.js/,"")} is ready!`);
-    });
-
-    await stat(join(__dirname, "../config.json"))
-    .catch(err => {
-        readFile(join(__dirname, "../config.template.json"), "utf8")
-        .then(res => {
-            writeFile(join(__dirname, "../config.json"), res)
-            console.log("config created")
-        })
-    })
-    .then(async () => {
-        const config: guildconfig.RootObject = JSON.parse(await readFile(join(__dirname, "../config.json"), "utf8"))
-
-        const ids = client.guilds.cache.map(guild => guild.id).filter(id => !Object.keys(config.guilds).includes(id));
-    
-        for (const id of ids) {
-            const guild = await client.guilds.fetch(id, true, true);
-            const newServer = {
-                [id]: ({...(config).guilds["0"]})
-            }
-            newServer[id].name = guild.name
-            
-            writeFile(join(__dirname, "../config.json"), JSON.stringify({ guilds: {...Object.assign(config.guilds, newServer)} }))
-            .catch(err => console.error(err));
-        }
-    })
+deepsearch(join(__dirname + "/Events/"), (file) => {
+    require(file).default();
+    console.log(`Event ${(file.split("\\")[file.split("\\").length - 1] ?? file.split("/")[file.split("/").length - 1]).replace(/\.js/,"")} is ready!`);
 });
 
 
