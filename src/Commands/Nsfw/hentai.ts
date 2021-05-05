b64b622999436cfbef1d6693f23c42f7ad20c9f3
fate <66224951+fatesc@users.noreply.github.com>
2021-05-05 19:50:15 +0100

    check for args

--- src/Commands/Nsfw/hentai.ts
+++ src/Commands/Nsfw/hentai.ts
@@ -1,24 +1,26 @@
 import fetch from "node-fetch";
 import { Message } from "discord.js";
 import { Command } from "../../types";
+import { helpCommand } from "../Util/HelpCommand";
 
 module.exports = {
     name: "hentai",
     description: "gives you hentai with amount (optional)",
-    usage: "hentai [amount?/tags?]",
+    usage: "hentai [amount?/tag1?] [tag2?] [tag3?] ...",
     nsfw: true,
     cooldown: 2,
     guildOnly: true,
     run(message: Message, args: string[]){
         const amount = +args[0]
+        let tags
         if (!!amount) {
             args.shift();
+            tags = args.join(",");
         }
-        const tags = args.join(",");
         
         if (!!amount && amount > 3 && !message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(`max is 3!! ${message.member}`);
         
-        fetch(`https://api.r34.app/booru/gelbooru/posts?baseEndpoint=rule34.xxx&limit=${!amount ? 1 : amount}&tags=${tags}&tagsEndpoint=/autocomplete.php&defaultQueryIdentifiersTagsTag=q&defaultQueryIdentifiersTagsTagEnding=`, {
+        fetch(`https://api.r34.app/booru/gelbooru/posts?baseEndpoint=rule34.xxx&limit=${!amount ? 1 : amount}${tags ? "&tags=" + tags : ""}&tagsEndpoint=/autocomplete.php&defaultQueryIdentifiersTagsTag=q&defaultQueryIdentifiersTagsTagEnding=`, {
             method: "GET", 
         })
         .then(res => res.json())
@@ -28,6 +30,8 @@ module.exports = {
             for (let i = 0; i < files.length; i += 4) {
                 message.inlineReply(`${files.slice(i, i + 4).join(" ")}`);
             }
+        }, r => {
+            message.inlineReply(r)
         })
     }
 } as Command
